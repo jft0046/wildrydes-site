af998d7df5d433b2f8557a2492f6c5f51de9cbf0
jft0046 <145061185+jft0046@users.noreply.github.com>
2024-04-19 23:42:32 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_Zt9uZeyiu',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '1cs2a51aglr5qnoafk4js02mve',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
